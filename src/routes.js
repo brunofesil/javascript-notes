c7eec3d727892a38e287b26abadd4038700dc46e
brunofesil <brfsilva@outlook.com>
2021-11-24 19:31:06 -0300

    Criadas rotas protegidas para /users e /notas

--- src/routes.js
+++ src/routes.js
@@ -7,14 +7,16 @@ import LoginScreen from './screens/auth/login';
 import NotesIndex from './screens/notes/index';
 import UsersEditScreen from './screens/users/edit';
 
+import PrivateRouter from './components/auth/private_router';
+
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Home} />
       <Route exact path='/register' component={RegisterScreen} />
       <Route exact path='/login' component={LoginScreen} />
-      <Route exact path='/notes' component={NotesIndex} />
-      <Route exact path='/users/edit' component={UsersEditScreen} />
+      <PrivateRouter exact path='/notes' component={NotesIndex} />
+      <PrivateRouter exact path='/users/edit' component={UsersEditScreen} />
     </Switch>
   </BrowserRouter>
 )
